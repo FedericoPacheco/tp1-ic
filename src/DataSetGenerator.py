a6d7d26730d2939f9dc6a75f221db8db88dc9a7e
FedericoPacheco <fedepacheco2112@gmail.com>
2023-09-07 21:03:43 -0300

    Mejorar expresiones regulares y guardar resultados a csv

--- src/DataSetGenerator.py
+++ src/DataSetGenerator.py
@@ -14,45 +14,50 @@ python -m spacy download en
 python -m spacy download en_core_web_md
 """
 
-TRAINING_FILE = "twitter_training.csv"
-VALIDATION_FILE = "twitter_validation.csv"
+TRAINING_AND_VALIDATION_FILE = "twitter_training.csv"
+TESTING_FILE = "twitter_validation.csv"
 CHOSEN_ENTITY = "Google"
 
 
 def get_dataset():
-    headers = ["id", "entity", "sentiment", "tweet"]
-    # Leer del csv asignando nombres a las columnas y descartando la primera (id)
+    # Cambiar al directorio del dataset 
+    os.chdir("..")
     os.chdir(os.path.join(os.getcwd(), "dataset"))
-    training_df = pd.read_csv(TRAINING_FILE, names = headers, usecols = headers[1:])
-    validation_df = pd.read_csv(VALIDATION_FILE, names = headers, usecols = headers[1:])
+    
+    # Leer del csv asignando nombres a las columnas y descartando la primera (id)
+    headers = ["id", "entity", "sentiment", "tweet"]
+    training_and_validation_df = pd.read_csv(TRAINING_AND_VALIDATION_FILE, names = headers, usecols = headers[1:])
+    testing_df = pd.read_csv(TESTING_FILE, names = headers, usecols = headers[1:])
     
     # Quedarse solo con la entidad de interes
-    training_df = training_df[training_df["entity"] == CHOSEN_ENTITY]
-    validation_df = validation_df[validation_df["entity"] == CHOSEN_ENTITY]
+    training_and_validation_df = training_and_validation_df[training_and_validation_df["entity"] == CHOSEN_ENTITY]
+    testing_df = testing_df[testing_df["entity"] == CHOSEN_ENTITY]
 
-    return training_df, validation_df
+    return training_and_validation_df, testing_df
 
 
 def preprocess_tweets(df):
     # Carga modelo para nlp (instalar previamente)
     nlp = spacy.load("en_core_web_md")
     
-    # Sacar caracteres molestos
+    # Sacar cosas innecesarias del texto
     def clean_text(raw_text):
         text = str(raw_text)                                # Convertir a cadena por si acaso
         text = contractions.fix(text)                       # Sacar abreviaturas
         text = emoji.demojize(text)                         # Sacar los emojis
+        text = re.sub(r"(http|www)\S+", "", text)           # Remover urls
         text = text.lower()                                 # Llevar todo a minuscula
         text = re.sub(r"\d+", "", text)                     # Remover numeros
         text = re.sub(r"[\t\n\r\f\v]", "", text)            # Remover enters y otras "porquerias"
         text = re.sub(r"[\.\,:;]", " ", text)               # Remover caracteres de puntuacion innecesarios
-        text = re.sub(r"[\[\]\(\)\{\}]", " ", text)
-        text = re.sub(r"[\"\´\`\'“”<>]", " ", text)
-        text = re.sub(r"[¿\?¡\!\@_~\+\*°#%\|\-\$]", " ", text)   
-        
+        text = re.sub(r"[\[\]\(\)\{\}]", "", text)
+        text = re.sub(r"[\"´`'′’“”<>]", "", text)
+        text = re.sub(r"[¿\?¡\!\@_~\+\*°#%\|\-\$/&–—…]", "", text)   
+        text = re.sub(r"\s{2,}", " ", text)                # Remover espacios de más   
+
         return text
     
-    # Operar "vectorialmente" sobre el data frame: limpiar texto, armar un doc de spacy con nlp(), fijarse que no es stopword y lematizar
+    # Operar "vectorialmente" sobre el data frame: limpiar texto, armar un doc de spacy con nlp(), fijarse que no sea stopword y lematizar
     df["tweet_preproc"] = df["tweet"].apply(
         lambda text: " ".join([token.lemma_ for token in nlp(clean_text(text)) if not token.is_stop])
     )
@@ -60,13 +65,25 @@ def preprocess_tweets(df):
     return df
     
 
-if __name__ == "__main__":
-    training_df, validation_df = get_dataset()
+def save_to_disk(df, file_name):
+    new_file_name = file_name.rsplit(".", 1)[0] + "-preproc.csv"
+    df[["sentiment", "tweet_preproc"]].to_csv(new_file_name, index = False)
 
-    preprocess_tweets(training_df)
-    preprocess_tweets(validation_df)
-    
-    print(training_df)
-    print(validation_df)
+
+def main():
+    training_and_validation_df, testing_df = get_dataset()
+
+    preprocess_tweets(training_and_validation_df)
+    preprocess_tweets(testing_df)
+
+    save_to_disk(training_and_validation_df, TRAINING_AND_VALIDATION_FILE)
+    save_to_disk(testing_df, TESTING_FILE)
+
+    #print(training_and_validation_df)
+    #print(testing_df)
+
+
+if __name__ == "__main__":
+    main()
 
 
